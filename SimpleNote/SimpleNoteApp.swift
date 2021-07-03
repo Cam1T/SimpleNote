5b7c97c9ddee66abf59c9d0f819daf664cf0850a
Cameron Tawiah <camerontawiah@yahoo.co.uk>
2021-07-03 16:22:30 +0100

    Created the project views

---
+++ SimpleNote/SimpleNoteApp.swift
@@ -0,0 +1,19 @@
+//
+//  SimpleNoteApp.swift
+//  SimpleNote
+//
+//  Created by Cam1T on 03/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct SimpleNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ListView()
+            }
+        }
+    }
+}
